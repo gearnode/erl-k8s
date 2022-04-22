3bdb82021d788e4f036a4fb7a0881ff452a22444
Nicolas Martyanoff <nicolas.martyanoff@exograd.com>
2022-04-22 17:19:19 +0200

    add a function to parse memory usage strings

--- src/k8s_units.erl
+++ src/k8s_units.erl
@@ -14,7 +14,7 @@
 
 -module(k8s_units).
 
--export([parse_cpu_usage/1]).
+-export([parse_cpu/1, parse_memory/1]).
 
 -export_type([error_reason/0]).
 
@@ -24,27 +24,27 @@
       | {invalid_value, binary()}
       | {invalid_unit, binary()}.
 
--spec parse_cpu_usage(binary()) -> {ok, float()} | {error, error_reason()}.
-parse_cpu_usage(<<>>) ->
+-spec parse_cpu(binary()) -> {ok, float()} | {error, error_reason()}.
+parse_cpu(<<>>) ->
   {error, empty_string};
-parse_cpu_usage(String) ->
+parse_cpu(String) ->
   Options = [{capture, all_but_first, binary}],
   case re:run(String, "^([0-9\.]+)([A-Za-z]+)?$", Options) of
     {match, [ValueString]} ->
-      parse_value(ValueString, 1.0);
+      parse_cpu_value(ValueString, 1.0);
     {match, [ValueString, <<"m">>]} ->
-      parse_value(ValueString, 1.0e-3);
+      parse_cpu_value(ValueString, 1.0e-3);
     {match, [ValueString, <<"n">>]} ->
-      parse_value(ValueString, 1.0e-9);
+      parse_cpu_value(ValueString, 1.0e-9);
     {match, [_ValueString, Unit]} ->
       {error, {invalid_unit, Unit}};
     nomatch ->
       {error, invalid_format}
   end.
 
--spec parse_value(binary(), float()) ->
+-spec parse_cpu_value(binary(), float()) ->
         {ok, float()} | {error, error_reason()}.
-parse_value(ValueString, Unit) ->
+parse_cpu_value(ValueString, Unit) ->
   try
     Value =
       case string:find(ValueString, <<".">>) of
@@ -54,6 +54,62 @@ parse_value(ValueString, Unit) ->
           erlang:binary_to_float(ValueString)
       end,
     {ok, Value * Unit}
+  catch
+    error:_ ->
+      {error, {invalid_value, ValueString}}
+  end.
+
+-spec parse_memory(binary()) ->
+        {ok, integer()} | {error, error_reason()}.
+parse_memory(<<>>) ->
+  {error, empty_string};
+parse_memory(String) ->
+  Options = [{capture, all_but_first, binary}],
+  case re:run(String, "^([0-9\.]+)([A-Za-z]+)?$", Options) of
+    {match, [ValueString]} ->
+      parse_memory_value(ValueString, 1);
+    {match, [ValueString, <<"k">>]} ->
+      parse_memory_value(ValueString, 1_000);
+    {match, [ValueString, <<"M">>]} ->
+      parse_memory_value(ValueString, 1_000_000);
+    {match, [ValueString, <<"G">>]} ->
+      parse_memory_value(ValueString, 1_000_000_000);
+    {match, [ValueString, <<"T">>]} ->
+      parse_memory_value(ValueString, 1_000_000_000_000);
+    {match, [ValueString, <<"P">>]} ->
+      parse_memory_value(ValueString, 1_000_000_000_000_000);
+    {match, [ValueString, <<"E">>]} ->
+      parse_memory_value(ValueString, 1_000_000_000_000_000_000);
+    {match, [ValueString, <<"Ki">>]} ->
+      parse_memory_value(ValueString, 1_024);
+    {match, [ValueString, <<"Mi">>]} ->
+      parse_memory_value(ValueString, 1_048_576);
+    {match, [ValueString, <<"Gi">>]} ->
+      parse_memory_value(ValueString, 1_073_741_824);
+    {match, [ValueString, <<"Ti">>]} ->
+      parse_memory_value(ValueString, 1_099_511_627_776);
+    {match, [ValueString, <<"Pi">>]} ->
+      parse_memory_value(ValueString, 1_125_899_906_842_624);
+    {match, [ValueString, <<"Ei">>]} ->
+      parse_memory_value(ValueString, 1_152_921_504_606_846_976);
+    {match, [_ValueString, Unit]} ->
+      {error, {invalid_unit, Unit}};
+    nomatch ->
+      {error, invalid_format}
+  end.
+
+-spec parse_memory_value(binary(), integer()) ->
+        {ok, float()} | {error, error_reason()}.
+parse_memory_value(ValueString, Unit) ->
+  try
+    Value =
+      case string:find(ValueString, <<".">>) of
+        nomatch ->
+          erlang:binary_to_integer(ValueString)*1.0;
+        _ ->
+          erlang:binary_to_float(ValueString)
+      end,
+    {ok, erlang:ceil(Value * Unit)}
   catch
     error:_ ->
       {error, {invalid_value, ValueString}}
--- test/k8s_units_tests.erl
+++ test/k8s_units_tests.erl
@@ -16,10 +16,10 @@
 
 -include_lib("eunit/include/eunit.hrl").
 
-parse_cpu_usage_test_() ->
+parse_cpu_test_() ->
   Parse =
     fun (String) ->
-        k8s_units:parse_cpu_usage(unicode:characters_to_binary(String))
+        k8s_units:parse_cpu(unicode:characters_to_binary(String))
     end,
   [?_assertEqual({ok, 0.0},
                  Parse("0")),
@@ -33,6 +33,32 @@ parse_cpu_usage_test_() ->
                  Parse("100m")),
    ?_assertEqual({ok, 0.25},
                  Parse("250000000n")),
+   ?_assertEqual({error, empty_string},
+                 Parse("")),
+   ?_assertEqual({error, invalid_format},
+                 Parse("foo")),
+   ?_assertEqual({error, {invalid_unit, <<"t">>}},
+                 Parse("2t"))].
+
+parse_memory_test_() ->
+  Parse =
+    fun (String) ->
+        k8s_units:parse_memory(unicode:characters_to_binary(String))
+    end,
+  [?_assertEqual({ok, 0},
+                 Parse("0")),
+   ?_assertEqual({ok, 100},
+                 Parse("100")),
+   ?_assertEqual({ok, 5_000},
+                 Parse("5k")),
+   ?_assertEqual({ok, 1_500_000},
+                 Parse("1.5M")),
+   ?_assertEqual({ok, 1_572_864},
+                 Parse("1.5Mi")),
+   ?_assertEqual({ok, 2_000_000},
+                 Parse("0.002G")),
+   ?_assertEqual({ok, 2_147_484},
+                 Parse("0.002Gi")),
    ?_assertEqual({error, empty_string},
                  Parse("")),
    ?_assertEqual({error, invalid_format},
